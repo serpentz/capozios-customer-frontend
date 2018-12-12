210b10689ce425426074da36078292d1d9cea3f8
nachoespino53 <nachoespino53@gmail.com>
2018-12-12 15:04:49 -0500

    Added Reducers and Actions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bulma';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import capozios from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(capozios);
+
+ReactDOM.render(
+    <Provider store = {store} >
+        <App />
+    </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+export default function capozios(state = [], action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
