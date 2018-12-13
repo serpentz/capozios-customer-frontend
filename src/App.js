a1ff73790c43b21cfb0a6d5da285595af81b8a89
nachoespino53 <nachoespino53@gmail.com>
2018-12-12 19:01:38 -0500

    Added React Router

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Login /> */}
+        <Login />
         <NavBar />
-        <SignUp />
+        {/* <SignUp /> */}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// Components
+import App from './App';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+// CSS
 import 'bulma';
+import './index.css';
+
+// Redux
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
+// React Router
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Imported Reducer
 import capozios from './reducers/index';
 
 const store = createStore(capozios);
 
 ReactDOM.render(
     <Provider store = {store} >
-        <App />
+        <Router>
+            <React.Fragment>
+                <Route exact path='/' component = {App} />
+                <Route exact path='/login' component = {Login} />
+                <Route exact path='/signup' component = {SignUp} />
+            </React.Fragment>
+        </Router>
     </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
