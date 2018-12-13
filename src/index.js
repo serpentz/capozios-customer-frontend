8b8434de72fb092c652476f8972e5d3b434fea0d
nachoespino53 <nachoespino53@gmail.com>
2018-12-12 19:20:30 -0500

    Thunk Added

--- src/index.js
+++ src/index.js
@@ -14,15 +14,18 @@ import './index.css';
 
 // Redux
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 // React Router
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // Imported Reducer
-import capozios from './reducers/index';
+import rootReducer from './reducers/index';
 
-const store = createStore(capozios);
+// ?
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store = {store} >
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,7 @@
-export default function capozios(state = [], action) {
+import { combineReducers } from 'redux';
+
+
+export default function rootReducer(state = [], action) {
     switch (action.type) {
         default:
             return state;
